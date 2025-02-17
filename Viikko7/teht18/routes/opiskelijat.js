27317521959e3d62f7fb8717a1bac5848936e5d5
niklaskamarainen <niklas.kamarainen@gmail.com>
2025-02-17 16:24:20 +0200

    tehtavat 17 ja 18 valmis

---
+++ Viikko7/teht18/routes/opiskelijat.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const router = express.Router();
+const opintorekisteri = require('../models/opintorekisteri_model');
+
+
+//Opiskelija
+router.get('/',
+    function (request, response) {
+        opintorekisteri.getAllStudents(function (err, dbResult) {
+            if (err) {
+                response.json(err);
+            } else {
+                console.log(dbResult);
+                response.json(dbResult);
+            }
+        })
+    });
+
+router.get('/:id',
+    function (request, response) {
+        opintorekisteri.getStudentsById(request.params.id, function (err, dbResult) {
+            if (err) {
+                response.json(err);
+            } else {
+                response.json(dbResult);
+            }
+        })
+    });
+
+
+router.post('/', 
+function(request, response) {
+  opintorekisteri.addStudent(request.body, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult);
+    }
+  });
+});
+
+
+router.delete('/:id', 
+function(request, response) {
+  opintorekisteri.deleteStudent(request.params.id, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult);
+    }
+  });
+});
+
+
+router.put('/:id', 
+function(request, response) {
+  opintorekisteri.updateStudent(request.params.id, request.body, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult);
+    }
+  });
+
+  
+});
+
+module.exports = router;
